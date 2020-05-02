ba785db9938e789b88bfad67764e67459290b6a1
1191073896 <1191073896@qq.com>
2020-05-02 12:34:12 +0800

    feature:timestamp format

--- logrotate.go
+++ logrotate.go
@@ -269,8 +269,8 @@ func FilterFileList(path, pat string) ([]string, error) {
 
 // getTimeStamp get time stamp
 func getTimeStamp() string {
-	now := time.Now().Format("2006.01.02.15.04.05.000")
-	timeSlot := strings.Replace(now, ".", "", -1)
+	now := time.Now().Format("2006.01.02.15.04.05")
+	timeSlot := strings.Replace(now, ".", "-", -1)
 	return timeSlot
 }
 
